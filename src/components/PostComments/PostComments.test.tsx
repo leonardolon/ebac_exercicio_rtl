89875aa1ee63a8b9c253bec95fd4ec254c789088
leonardo <leonardo.lon@outlook.com>
2024-06-21 19:54:33 -0300

    final test

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+    it('Deve permitir adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
+        fireEvent.submit(screen.getByText('Comentar'));
+
+        fireEvent.change(input, { target: { value: 'Segundo comentário' } });
+        fireEvent.submit(screen.getByText('Comentar'));
+
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
